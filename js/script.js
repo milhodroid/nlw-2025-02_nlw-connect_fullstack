950ecb18d3f1c7a45315c219ece945cea9d9d420
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2025-02-21 22:48:11 -0300

    aula 03

--- js/script.js
+++ js/script.js
@@ -35,14 +35,30 @@ const getTotalSubscribers = (userData) => {
 
 const showInvite = (userData) => {
     app.innerHTML = `
-        <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled />
-        <div id="status">
-            <h4>${getTotalSubscribers(userData)}</h4>
+        <main>
+            <h3>Inscrição confirmada!</h3>
+
             <p>
-                Inscrições feitas
+                Convide mais pessoas e concorra a prêmios!
+                <br >
+                Compartilhe o link e acompanhe as inscrições:
             </p>
-        </div>
+
+            <div class="input-group">
+                <label for="link">
+                    <img src="link.svg" alt="Link icon">
+                </label>
+                <input type="text" name="link" id="link" value="https://evento.com?ref=${userData.ref}" disabled />
+            </div>
+        </main>
+
+        <section class="stats">
+            <h4>${getTotalSubscribers(userData)}</h4>
+            <p>Inscrições feitas</p>
+        </section>
     `
+    app.setAttribute('class', 'page-invite')
+    updateImageLinks()
 }
 
 const saveUser = (userData) => {
@@ -53,7 +69,6 @@ const saveUser = (userData) => {
     }
 
     users.push(newUser)
-    console.log(users)
     return newUser
 }
 
@@ -77,19 +92,61 @@ const formAction = () => {
     }
 }
 
+const updateImageLinks = () => {
+    document.querySelectorAll('img').forEach((img) => {
+        if(img.src.includes('assets')) {
+            return
+        }
+        const src = img.getAttribute('src')
+        img.src = `assets/img/${src}`
+    })
+}
+
 const startApp = () => {
     const content = `
-        <form id="form">
-            <input type="email" name="email" placeholder="E-mail" />
-            <input type="text" name="phone" placeholder="Telefone" />
-            <button>
-                Confirmar
-            </button>
-        </form>
+        <main>
+            <section class="about">
+                <div class="section-header">
+                    <h2>Sobre o evento</h2>
+                    <span class="badge">AO VIVO</span>
+                </div>
+
+                <p>
+                    Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestrar, workshops e hackathons.
+                    <br /><br />
+                    Dias 15 a 17 de Março | das 18h às 21h | Online & gratuito
+                </p>
+            </section>
+
+            <section class="registration">
+                <h2>Inscrição</h2>
+                <form id="form">
+                    <div class="input-wrapper">
+                        <div class="input-group">
+                            <label for="email">
+                                <img src="mail.svg" alt="Email icon">
+                            </label>
+                            <input type="email" name="email" id="email" placeholder="E-mail">
+                        </div>
+                        <div class="input-group">
+                            <label for="phone">
+                                <img src="phone.svg" alt="Phone icon">
+                            </label>
+                            <input type="text" name="phone" id="phone" placeholder="Telefone">
+                        </div>
+                    </div>
+                    <button>
+                        Confirmar
+                        <img src="arrow.svg" alt="Arrow right" />
+                    </button>
+                </form>
+            </section>
+        </main>
     `
 
     app.innerHTML = content
-
+    app.setAttribute('class', 'page-start')
+    updateImageLinks()
     formAction()
 }
 
